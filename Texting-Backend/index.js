3bfce6587f0ee3b6c93661f56ef9f39f9d4c4f93
Uday <udayrajveerjain@gmail.com>
2025-02-17 13:44:03 +1300

    Introduced A Salting and hashing function to the code

--- Texting-Backend/index.js
+++ Texting-Backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 const app = express();
 const port = 3000;
 
@@ -8,15 +9,37 @@ let users = [];
 app.use(cors());
 app.use(express.json());
 
+async function hashPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+
+  return hashPassword;
+}
+
 app.get('/api/users', (request, response) => {
   response.json(users);
 });
 
-app.post('/api/users', (request, response) => {
+app.post('/api/users', async (request, response) => {
   const body = request.body;
+
+  if (!body.email || !body.password || !body.firstName) {
+    return response.status(400).json({
+      error: 'missing email, password or firstName',
+    });
+  }
+
+  if (users.find((user) => user.email === body.email)) {
+    return response.status(400).json({
+      error: 'email must be unique',
+    });
+  }
+
+  const hashedPw = await hashPassword(body.password);
+
   const User = {
     email: body.email,
-    password: body.password,
+    password: hashedPw,
     firstName: body.firstName,
   };
 
