5d7babb1f532cc36fe388c24cb8dd7dac530c071
Uday <udayrajveerjain@gmail.com>
2025-02-26 18:06:51 +1300

    Created Message Controller

---
+++ Texting-Backend/controllers/messageController.js
@@ -0,0 +1,37 @@
+const Message = require('../models/messageDetails');
+
+const createMessage = async (req, res) => {
+  const { message, sender, date } = req.body;
+
+  if (!message || !sender || !date) {
+    return res.status(400).json({
+      error: 'Message, sender and date are required',
+    });
+  }
+  try {
+    const message = new Message({
+      message: message,
+      sender: sender,
+      date: date,
+    });
+
+    const savedMessage = await message.save();
+    res.json(savedMessage);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const getMessages = async (req, res) => {
+  try {
+    const messages = Message.find({});
+    res.json(messages);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  getMessages,
+  createMessage,
+};
